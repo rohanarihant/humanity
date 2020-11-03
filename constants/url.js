bbd96f009d9e338167bb9f8981c2dd8a1bc6ce95
Rohan <rohan.arihant@gmail.com>
2020-11-03 19:57:22 +0530

    resolved some issues

--- constants/url.js
+++ constants/url.js
@@ -1,1 +1,2 @@
-export const API_URLS = 'http://humanitydemo.rubrutech.com/';
+// export const API_URLS = 'http://humanitydemo.rubrutech.com/';
+export const API_URLS = 'https://humanity.rubrutech.com/';
