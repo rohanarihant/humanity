9d2fbf9294de4031361f18caa803eb375287d7f2
Rohan <rohan.arihant@gmail.com>
2020-11-09 13:01:36 +0530

    url change

--- constants/url.js
+++ constants/url.js
@@ -1,2 +1,2 @@
-export const API_URLS = 'http://humanitydemo.rubrutech.com/';
-// export const API_URLS = 'https://humanity.rubrutech.com/';
+// export const API_URLS = 'http://humanitydemo.rubrutech.com/';
+export const API_URLS = 'https://humanity.rubrutech.com/';
